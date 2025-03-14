68a9a1f281d99b63eb017ead9303da8a03e12bd5
jorgen <jorgen@DESKTOP-BLMTEID>
2025-03-14 18:20:50 +0100

    Add project files.

---
+++ DataModels/Patient.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace PatientDatabaseWebApp.DataModels
+{
+    public class Patient
+    {
+        public int Id { get; set; }
+
+        public string? Name { get; set; }
+
+        public DateTime DateOfBirth { get; set; }
+
+        public string? Conditions { get; set; }
+    }
+}
