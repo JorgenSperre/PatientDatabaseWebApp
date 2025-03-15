2a2c3afb53af879080f4fe6b8d07c2faab55fa07
Jorgen Sperre <jorgen.sperre@gmail.com>
2025-03-15 13:02:42 +0100

    Added a FindAge() method in the Patient class, and re-scaffolded the model to get updated PatientPages.

--- DataModels/Patient.cs
+++ DataModels/Patient.cs
@@ -11,7 +11,21 @@ namespace PatientDatabaseWebApp.DataModels
         public string? Name { get; set; }
 
         public DateTime DateOfBirth { get; set; }
+        public int Age { get; set; }
 
         public string? Conditions { get; set; }
+
+        public void FindAge()
+        {
+            DateTime today = DateTime.Today;
+            var years = today.Year - this.DateOfBirth.Year;
+
+            if (this.DateOfBirth.Date > today.AddYears(-years))
+            {
+                years--;
+            }
+
+            this.Age = years;
+        }
     }
 }
---
+++ Migrations/20250315115001_UpdatePatientClass.cs
@@ -0,0 +1,47 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace PatientDatabaseWebApp.Migrations
+{
+    /// <inheritdoc />
+    public partial class UpdatePatientClass : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "Name",
+                table: "Patient",
+                type: "nvarchar(max)",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)");
+
+            migrationBuilder.AddColumn<int>(
+                name: "Age",
+                table: "Patient",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Age",
+                table: "Patient");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Name",
+                table: "Patient",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "",
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)",
+                oldNullable: true);
+        }
+    }
+}
